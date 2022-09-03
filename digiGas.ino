1980d730d565753f7b0452cc2d382eb05a697a38
Jessica Lartey <coloneltiorkor@gmail.com>
2022-09-03 14:49:57 +0000

    wifi and thingspeak configurations

--- digiGas.ino
+++ digiGas.ino
@@ -1,70 +1,119 @@
-#include<SoftwareSerial.h>
-#include<LiquidCrystal.h>
+#include <Wire.h>
+//#include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>
+#include "ThingSpeak.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+//wifi config
+const char* ssid = "Brielle"; //wifi ssid
+const char* password = "sweetlittleMaryJane"; //wifi password
+
+//thing speak config
+const unsigned long channel_id = 1848630;
+const char* write_api_key = "48JZEEL1FKM9WTY2";
+
+//wifi client object
+WiFiClient client;
 
 //define pins
-int gasSensor = A1;
-int flameSensor = 2;
-int led = 4;
-int buzzer = 3;
+int gasSensor = 12;  //D6
+int flameSensor = 14;  //D5
+int led = 13;  //D7
+int buzzer = 15;  //D8
 
-float gasValue;
-float gasThreshold = 50;
-float fireValue;
+int gasValue;
+int gasThreshold = 100;
+int fireValue;
+
+//*****lcd pins: SDA to D2, SCL to D1
 
 void setup() {
-  Serial.begin(9600);
   pinMode(gasSensor, INPUT);
   pinMode(flameSensor, INPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(led, OUTPUT);
+  lcd.begin();
+  lcd.clear();
 
-  //lcd messages
-  String gas =  "Alert! Gas detected";
-  String fire =  "Alert! Fire detected";
-  String gasAndFire =  "Alert! Gas and fire detected"
+  //connect to thing speak
+  ThingSpeak.begin(client);
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Wifi connecting to ");
+  Serial.println(ssid);
   
+  WiFi.begin(ssid, password);
+  
+  Serial.println();
+  Serial.print("connecting");
+  
+  while(WiFi.status()!= WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+    }
+   Serial.println();
+  
+   Serial.println("Wifi connected");
+   delay(500);
+   Serial.print("node mcu IP address: ");
+   Serial.print(WiFi.localIP());
+
+   randomSeed(micros());
 }
 
 void loop() {
   gasValue = analogRead(gasSensor);
   fireValue = digitalRead(flameSensor);
-  /*
-  Serial.print("Gas Status: ");
-  Serial.print(gasValue);
-  Serial.println(" ");
-  Serial.print("Fire Status: ");
-  Serial.print("fireValue");
-  Serial.println(" ");
-  delay(100);
-*/
-  if (gasValue < gasThreshold && fireValue == HIGH){
-    //digitalWrite(buzzer, HIGH);
-    tone(buzzer, 5000); 
-    delay(500);
-    noTone(buzzer);
-    delay(500);
+  Serial.println(fireValue);
+
+   //lcd messages
+  String gasMessage =  "Alert! Gas detected";
+  String fireMessage =  "Alert! Fire detected";
+  String gasAndFireMessage =  "Alert! Gas and fire detected";
+
+  ThingSpeak.setField(1, gasValue);
+  ThingSpeak.setField(2, fireValue);
+  
+  if (gasValue < gasThreshold && fireValue == LOW){
     digitalWrite(led, HIGH);
-    Serial.print("Alert! Fire detected"); //to be printed on lcd and sent to user
-    }
-  else if (gasValue > gasThreshold && fireValue == LOW){
-    digitalWrite(buzzer, HIGH);
-    tone(buzzer, 5000); 
-    delay(500);
+    tone(buzzer, 1000); 
+    delay(1000);
+    digitalWrite(led, LOW);
     noTone(buzzer);
-    delay(500);
-    digitalWrite(led, HIGH);
-    Serial.print("Alert! Gas detected"); //to be printed on lcd and sent to user
-    }
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(fireMessage);
+   }
   else if (gasValue > gasThreshold && fireValue == HIGH){
-    digitalWrite(buzzer, HIGH);
-    tone(buzzer, 5000); 
-    delay(500);
-    noTone(buzzer);
-    delay(500);
     digitalWrite(led, HIGH);
-    Serial.print("Alert! Gas and fire detected"); //to be printed on lcd and sent to user
-    }
+    tone(buzzer, 1000); 
+    delay(1000);
+    digitalWrite(led, LOW);
+    noTone(buzzer);
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(gasMessage);
+   }
+  else if (gasValue > gasThreshold && fireValue == LOW){
+    digitalWrite(led, HIGH);
+    tone(buzzer, 1000); 
+    delay(1000);
+    digitalWrite(led, LOW);
+    noTone(buzzer);
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(gasAndFireMessage);
+   }
   else{
-    Serial.println("System Stable");
-    }  
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("system stable");
+    delay(500);
+   }  
 }
